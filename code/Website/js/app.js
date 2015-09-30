f7620d052a2fa19f173e661568c3dfe24d19809d
unknown <matt.bubernak@gmail.com>
2015-09-29 23:03:13 -0600

    recipes now populate with the ingredients inside of them.

--- code/Website/js/app.js
+++ code/Website/js/app.js
@@ -1,4 +1,7 @@
 var myApp = angular.module('myApp',['ngRoute']);
+
+// This is the routing configuring for our application. We have 3 pages, a main recipes/search page, an
+// ingredient listing page, and a page for creating recipes. 
 myApp.config(function ($routeProvider) {
 	$routeProvider 
 	.when('/', {
@@ -23,7 +26,8 @@ myApp.config(function ($routeProvider) {
 	})
 });
 
-
+// This service reads the data from our "database"(json files), and provides our controllers
+// access to it. When they access it, they are accessing a shared copy. 
 myApp.service('databaseService', function ($http,$q) {
 	var deferred1 = $q.defer();
 	var deferred2 = $q.defer(); 
@@ -46,28 +50,58 @@ myApp.service('databaseService', function ($http,$q) {
 		return deferred2.promise
 	}
 
+
 })
 
+// Our controller for the main recipes/search page. 
 myApp.controller('recipesController',function($scope,$http,databaseService) {
   $scope.nameSearchInput = ""
-	//Load data from the db
   var promise1 = databaseService.getIngredients(); 
   var promise2 = databaseService.getRecipes()
+
+  $scope.getIngrById = function (inID)
+  {
+  	for (var i = 0; i < $scope.ingredients.length; i++)
+  	{
+  		if ($scope.ingredients[i].id == inID)
+  			return $scope.ingredients[i];
+  	}
+	// should never hit this.
+	console.log(inId);
+	return -1; 
+  }
+
+  // Grab ingredient data
   promise1.then(function (data)
   {
   	$scope.ingredients = data.data;
+  	console.log("hi");
   })
+
+  // Grab recipe data
   promise2.then(function (data)
   {
   	$scope.recipes = data.data;
+  	// Modify recipe data to instead instances of the ingredients. 
+	for (var i = 0; i < $scope.recipes.length; i++)
+	{
+		for (var j = 0; j < $scope.recipes[i].ingredients.length; j++)
+		{
+			// Add an array of actual ingredient objects. 
+			$scope.recipes[i].ingredients[j] = $scope.getIngrById($scope.recipes[i].ingredients[j]);
+		}
+	}
   })
-	//$scope.ingredients = [{"name" : "peppermint","description" : "just nud"}]
+
 });
+
+// Our controller for the create page. 
 myApp.controller('createController', ['$scope','$log',
 function($scope,$location,$log) {
 
 }]);
 
+// Our controller for the ingredients page. 
 myApp.controller('ingredientsController', function($scope,$http,databaseService) {
   $scope.nameSearchInput = ""
   var promise = databaseService.getIngredients(); 
@@ -78,9 +112,12 @@ myApp.controller('ingredientsController', function($scope,$http,databaseService)
 
 });
 
+// Our controller for the form on the create page. 
 myApp.controller('formController', function($scope,databaseService) {
+	$scope.showSuccess = false;
 	$scope.newRecip = {name: "Name it", description: "Describe the blend..."}
 	var promise1 = databaseService.getIngredients(); 
+
 	promise1.then(function (data)
 	{
 		$scope.ingredients = data.data;
@@ -95,16 +132,13 @@ myApp.controller('formController', function($scope,databaseService) {
         $scope.newRecip = angular.copy($scope.master);
     };
     $scope.submit = function() {
-    	// add it to the db
-        //$scope.newRecip = angular.copy($scope.master);
-
         $scope.recipes.push({ 
 			"name" : $scope.newRecip.name,
 			"id" : $scope.ingredients.length,
 			"description" : $scope.newRecip.description,
 			"img" : "placeholder.png"
 		})
-    	console.log('lol');
+    	$scope.showSuccess = true;
     };
     $scope.reset();
 });
